c89c2837ac2f3ae769ac18e149d20ce9a80efd6f
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2014-04-09 17:09:40 +0200

    dash doc update

---
+++ dash-1.1/classdash_1_1_context.js
@@ -0,0 +1,13 @@
+var classdash_1_1_context =
+[
+    [ "Context", "classdash_1_1_context.html#a50d48925fbb32acf97fd8203d1991da5", null ],
+    [ "~Context", "classdash_1_1_context.html#a641e3dbb72f12fd3f20553519cc8e59f", null ],
+    [ "apply", "classdash_1_1_context.html#a15f85ac49d381dd280b872a6d800c7c0", null ],
+    [ "commit", "classdash_1_1_context.html#a1d1b08dc273e6300700475f85c6e75b9", null ],
+    [ "getImpl", "classdash_1_1_context.html#aa4d715607b39525efb27a29463151be4", null ],
+    [ "map", "classdash_1_1_context.html#aa80594adb5a6297c76b64ef022a97a80", null ],
+    [ "map", "classdash_1_1_context.html#ad4bd4d191ba5fbe673cee7990995a20d", null ],
+    [ "setCurrent", "classdash_1_1_context.html#a3f2213a0b2ebd9c16216cf503c428436", null ],
+    [ "unmap", "classdash_1_1_context.html#a860fecbac6d45e1f0389393efa86548f", null ],
+    [ "unmap", "classdash_1_1_context.html#a90b13b081d0abb92d207bf4b21407af2", null ]
+];
--- dash-1.1/search/classes_70.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['parentiterator',['ParentIterator',['../classdash_1_1detail_1_1_parent_iterator.html',1,'dash::detail']]]
-];
