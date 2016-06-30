d41a99cc3577ce8f7639518243918791ee92cfdb
BBP Release Manager <bbprelman@epfl.ch>
2016-07-01 01:06:04 +0200

    Updated Brayns 0.1

---
+++ Brayns-0.1/navtreedata.js
@@ -0,0 +1,38 @@
+var NAVTREE =
+[
+  [ "Brayns", "index.html", [
+    [ "Changelog", "_changelog.html", [
+      [ "Release 0.1 (2016-06-16)", "_changelog.html#Release010", null ]
+    ] ],
+    [ "Todo List", "todo.html", null ],
+    [ "Classes", null, [
+      [ "Class List", "annotated.html", "annotated" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"classbrayns_1_1_flying_mode_manipulator.html#ac471e9443421947513fa48082676740d",
+"classbrayns_1_1_scene.html#ab3fc6cbb00bd593a5140033635ff1fba",
+"structbrayns_1_1obj_1_1_extended_o_b_j_material.html#ac377b2acb4991b9b94804a8a6715e17b"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ Brayns-0.1/search/searchdata.js
@@ -0,0 +1,36 @@
+var indexSectionsWithContent =
+{
+  0: "_abcdefgilmoprstuvz",
+  1: "abcdefgilmoprstvz",
+  2: "av",
+  3: "_abcdefgimoprstu",
+  4: "_m",
+  5: "g",
+  6: "b",
+  7: "ct"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "related",
+  6: "defines",
+  7: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Friends",
+  6: "Macros",
+  7: "Pages"
+};
+
