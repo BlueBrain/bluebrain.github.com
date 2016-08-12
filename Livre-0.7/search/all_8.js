878e189d2607c82f21ce061dd73b16223aee59ea
BBP Release Manager <bbprelman@epfl.ch>
2016-08-12 03:05:29 +0200

    Updated Livre 0.7

--- Livre-0.7/search/all_8.js
+++ Livre-0.7/search/all_8.js
@@ -2,8 +2,6 @@ var searchData=
 [
   ['makecurrent',['makeCurrent',['../classlivre_1_1_g_l_context.html#a19ff075882cba25a29393a8af8e024c4',1,'livre::GLContext']]],
   ['maximumblocksize',['maximumBlockSize',['../structlivre_1_1_volume_information.html#a7f58df57b66e81d07df72c398c1b9dfd',1,'livre::VolumeInformation']]],
-  ['maxpos',['maxPos',['../structlivre_1_1_volume_information.html#a86e55a8e1dda4a5d3b1405860959c72a',1,'livre::VolumeInformation']]],
   ['memoryunit',['MemoryUnit',['../classlivre_1_1_memory_unit.html',1,'livre']]],
-  ['metertodataunitratio',['meterToDataUnitRatio',['../structlivre_1_1_volume_information.html#a4ac1a3924b611e51bf6b06e0a33b3793',1,'livre::VolumeInformation']]],
-  ['minpos',['minPos',['../structlivre_1_1_volume_information.html#a836aaca1c5f0132e037e49d2b28f16b7',1,'livre::VolumeInformation']]]
+  ['metertodataunitratio',['meterToDataUnitRatio',['../structlivre_1_1_volume_information.html#a4ac1a3924b611e51bf6b06e0a33b3793',1,'livre::VolumeInformation']]]
 ];
