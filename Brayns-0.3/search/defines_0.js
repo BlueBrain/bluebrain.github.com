02b2325d061668d355589fb612d750aecd1cfeac
BBP Release Manager <bbprelman@epfl.ch>
2017-07-13 01:21:17 +0200

    Updated Brayns 0.3

--- Brayns-0.3/search/defines_0.js
+++ Brayns-0.3/search/defines_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['brayns_5frev_5fstring',['BRAYNS_REV_STRING',['../version_8h.html#abdfd5d63377bd1606cf51a0437943637',1,'version.h']]],
   ['brayns_5fversion_5fabi',['BRAYNS_VERSION_ABI',['../version_8h.html#a689d3561485abd45ee50411137295ee0',1,'version.h']]],
   ['brayns_5fversion_5fge',['BRAYNS_VERSION_GE',['../version_8h.html#a4453b9a10c85cfaf4e3a599a6105fcea',1,'version.h']]],
   ['brayns_5fversion_5fgt',['BRAYNS_VERSION_GT',['../version_8h.html#ae06db9b236b4f8ed1de4c3d1cbf55fe5',1,'version.h']]],
@@ -8,5 +9,6 @@ var searchData=
   ['brayns_5fversion_5fmajor',['BRAYNS_VERSION_MAJOR',['../version_8h.html#af6fac12cc372fa2c0d4a7276646db3e2',1,'version.h']]],
   ['brayns_5fversion_5fminor',['BRAYNS_VERSION_MINOR',['../version_8h.html#ad1d30abb92c583df9555702986df6cd5',1,'version.h']]],
   ['brayns_5fversion_5fpatch',['BRAYNS_VERSION_PATCH',['../version_8h.html#aec3b431747b4611d046a5be799c36a85',1,'version.h']]],
-  ['brayns_5fversion_5frevision',['BRAYNS_VERSION_REVISION',['../version_8h.html#a4d697b7dac7e97b28fecf7714fea4d8f',1,'version.h']]]
+  ['brayns_5fversion_5frevision',['BRAYNS_VERSION_REVISION',['../version_8h.html#a4d697b7dac7e97b28fecf7714fea4d8f',1,'version.h']]],
+  ['brayns_5fversion_5fstring',['BRAYNS_VERSION_STRING',['../version_8h.html#aae1fd1122c42331f598c0152baa6572a',1,'version.h']]]
 ];
