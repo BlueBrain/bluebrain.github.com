133b78071d14300a5836b9837cd20b92b2329aa1
BBP Release Manager <bbprelman@epfl.ch>
2018-03-08 01:33:45 +0100

    Updated Brayns 0.6

--- Brayns-0.6/classbrayns_1_1_deflect_plugin.js
+++ Brayns-0.6/classbrayns_1_1_deflect_plugin.js
@@ -1,6 +1,6 @@
 var classbrayns_1_1_deflect_plugin =
 [
-    [ "DeflectPlugin", "classbrayns_1_1_deflect_plugin.html#ae42193d578a62ae3169857dee5384edf", null ],
+    [ "DeflectPlugin", "classbrayns_1_1_deflect_plugin.html#a875f371d8d7cef572623e2e19d37c5bf", null ],
     [ "postRender", "classbrayns_1_1_deflect_plugin.html#a893434fe2333a2fba6b17b09cf74d0a2", null ],
     [ "preRender", "classbrayns_1_1_deflect_plugin.html#a0f1d7413458d8b505e5fa483b074e0e0", null ]
 ];
--- Brayns-0.6/classbrayns_1_1_rockets_plugin.js
+++ Brayns-0.6/classbrayns_1_1_rockets_plugin.js
@@ -1,6 +1,6 @@
 var classbrayns_1_1_rockets_plugin =
 [
-    [ "RocketsPlugin", "classbrayns_1_1_rockets_plugin.html#a16d33fa1ec280039f8d17ae55bb6827a", null ],
+    [ "RocketsPlugin", "classbrayns_1_1_rockets_plugin.html#a8cdda01dd30fe46faf5f52e8bf305db3", null ],
     [ "~RocketsPlugin", "classbrayns_1_1_rockets_plugin.html#a81fa3fa6fce0225ac15faee6274b1123", null ],
     [ "postRender", "classbrayns_1_1_rockets_plugin.html#a31a48fc03723237f4248519742ca3b54", null ],
     [ "preRender", "classbrayns_1_1_rockets_plugin.html#af77ed4ee68a41b6d46479377e1789815", null ]
