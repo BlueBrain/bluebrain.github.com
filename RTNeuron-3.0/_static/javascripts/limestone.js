ef1d352b624c5709853f7ff5900b8f5ce72cb49e
Andrew Hale <43584291+haleepfl@users.noreply.github.com>
2020-01-30 16:23:58 +0100

    Update RTNeuron docs for 2020

--- RTNeuron-3.0/_static/javascripts/limestone.js
+++ RTNeuron-3.0/_static/javascripts/limestone.js
@@ -1,32 +1,20 @@
-// © Blue Brain Project/EPFL 2005 – 2019. All rights reserved.
-
-// This file contains some fixes required as a result of the translation
-// between mkdocs and sphinx.
-// This uses an anonymous function rather than jQuery $(document).ready so that
-// it runs immediately, since it must modify the DOM before application.js which
-// needs the code divs fixed first.
+/*
+ * Copyright (c) Blue Brain Project/EPFL 2018 – 2019. All rights reserved.
+ */
 
 (function () {
   // Clipboard requires codehilite class not highlight
-  $("div.highlight")
-    .children("pre")
-    .wrap("<div class='codehilite'></div>");
-  $("div[class^='highlight-'], div[class*=' highlight-']")
-    .children()
-    .unwrap();
-  $("div.highlight")
-    .children()
-    .unwrap();
+  $("div.highlight").addClass("codehilite")
   // Tables are incorrectly rendered as the js/css requires they have no class.
-  $("table.docutils").not(".field-list")
+  $("table.docutils")
+    .not(".field-list")
     .removeAttr("border")
     .removeAttr("class");
   // Add inline code highlighting, users need to set:
   // .. role:: js(code)
   //    :language: js
   // and use like :js:`return target`
-  $("code.code")
-    .addClass("codehilite");
+  $("code.code").addClass("codehilite");
   // Add the required id attribute to headings so that they receive the correct
   // offsets. This also removes the id of the wrapping div from Sphinx.
   $("div.section[id]")
@@ -34,5 +22,10 @@
     .each(function(){
       $(this).attr("id", $(this).parent().attr("id"));
       $(this).parent().removeAttr("id");
+    });
+  // Resize sphinx-iframes to fit contents
+  $("iframe.sphinx-iframe")
+    .on("load", function(){
+      $(this).height($(this).contents().height());
     });
 })();
